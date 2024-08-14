9caff233c0ccd04054c633be6e56908a4e5dd600
Pathogenbeats <0933490@my.scccd.edu>
2024-08-14 11:53:42 -0700

    Add files via upload
    Uploaded my first C++ program

---
+++ helloWorld.cpp
@@ -0,0 +1,14 @@
+// Andrew Powell 8/14/2024
+// helloWorld.cpp
+/*
+ *This is my first c++ program
+ *It only took me an hour to write!
+ *
+ */
+#include <iostream>
+using namespace std;
+
+int main() {
+    cout << "\nHello, World!\n" << endl << "______________" << endl << "\nAnother String" << endl;
+    return 0;
+}
