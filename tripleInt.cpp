f5f48dfd9ffa7a303fbf199aae377ec14830d9b9
Pathogenbeats <0933490@my.scccd.edu>
2024-08-25 21:48:24 -0700

    Add files via upload
    So here is my Triple an Integer project hopefully I did these correctly. 8/25/2024

---
+++ tripleInt.cpp
@@ -0,0 +1,20 @@
+// Andrew Powell 8/25/2024
+// tripleInt.cpp
+
+#include <iostream>
+
+// I am glad that now that I have the doubleInt project figured out this was a whole lot easier
+using namespace std;
+
+int main() {
+    // I pretty much did the same code as doubleInt but, added the Triple
+
+    int num1;
+
+    cout << "\nThis is my Triple an int project! \nHopefully you didn't mean decimals!" << endl;
+    cout << "Enter A Number: " << endl;
+    cin >> num1;
+    cout << "Your number is: " << num1 << "\nYour number doubled is: " << num1 * 2
+    << "\nYour number tripled is: " << num1 * 3 << endl; ;
+    return 0;
+}
